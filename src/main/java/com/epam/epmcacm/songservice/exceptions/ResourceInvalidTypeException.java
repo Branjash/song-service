834c83b30c0882fc27863f4260cca79e11f15b66
eefbf81 <branko.rovcanin@mckesson.com>
2022-10-06 00:27:31 +0200

    fix song controller

--- src/main/java/com/epam/epmcacm/songservice/exceptions/ResourceInvalidTypeException.java
+++
@@ -1,9 +0,0 @@
-package com.epam.epmcacm.songservice.exceptions;
-
-public class ResourceInvalidTypeException extends  Exception{
-
-    public ResourceInvalidTypeException(String message)
-    {
-        super(message);
-    }
-}
--- src/main/java/com/epam/epmcacm/songservice/exceptions/ResourceS3Exception.java
+++
@@ -1,9 +0,0 @@
-package com.epam.epmcacm.songservice.exceptions;
-
-public class ResourceS3Exception extends  Exception{
-
-    public ResourceS3Exception(String message)
-    {
-        super(message);
-    }
-}
